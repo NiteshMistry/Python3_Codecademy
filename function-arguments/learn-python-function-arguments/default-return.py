d1de15206b6c574a2036dae8e59b02a4700c60cc
Nitesh Mistry <9368802+NiteshMistry@users.noreply.github.com>
2020-06-01 20:09:25 -0400

    Create default-return.py

---
+++ function-arguments/learn-python-function-arguments/default-return.py
@@ -0,0 +1,13 @@
+# store the result of this print() statement in the variable prints_return
+print("What does this print function return?")
+
+# print out the value of prints_return
+prints_return = print("hi!")
+print(prints_return)
+
+# call sort_this_list.sort() and save that in list_sort_return
+sort_this_list = [14, 631, 4, 51358, 50000000]
+
+# print out the value of list_sort_return
+list_sort_return = sort_this_list.sort()
+print(list_sort_return)
